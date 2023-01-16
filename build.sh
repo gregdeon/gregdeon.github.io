ed09a0d061abf8ea3e9759f90fed80b180394260
Greg d'Eon <greg.l.deon@gmail.com>
2023-01-15 22:26:27 -0800

    CV: move file into the right folder

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ python build.py --template template/index_template.html --publications template/
 # Build CV
 python build.py --template cv/cv_template.tex --publications template/publications.yaml --output cv/cv.tex --debug
 latexmk cv/cv.tex -pdf --outdir=cv --auxdir=cv/aux_files
+mv cv/cv.pdf files/cv.pdf
